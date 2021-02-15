0b4a21466e8a5f300a6af4251dd85911e282fc08
Darius Pirvulescu <organicdarius@gmail.com>
2021-02-15 23:52:30 +0200

    Add repositoriesReducer and annotate

---
+++ src/redux/reducers/repositoriesReducer.ts
@@ -0,0 +1,25 @@
+interface RepositoryState {
+  loading: boolean;
+  error: string | null;
+  data: string[]
+}
+
+interface Action {
+  type: string;
+  payload?: any
+}
+
+const repositoriesReducer = (state: RepositoryState, action: Action): RepositoryState => {
+  switch (action.type) {
+    case 'search_repositories':
+      return { loading: true, error: null, data: [] }
+    case 'search_repositories_success':
+      return { loading: false, error: null, data: action.payload }
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] }
+    default:
+      return state
+  }
+};
+
+export default repositoriesReducer;
