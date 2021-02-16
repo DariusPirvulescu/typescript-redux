0eef5a7caefabc6e1efdb70cc079298445a64d2b
Darius Pirvulescu <organicdarius@gmail.com>
2021-02-16 22:59:22 +0200

    Add better annotation for redux Action

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -1,24 +1,58 @@
 interface RepositoryState {
   loading: boolean;
   error: string | null;
-  data: string[]
+  data: string[];
 }
 
-interface Action {
-  type: string;
-  payload?: any
+// interface Action {
+//   type: string;
+//   payload?: any; // the 'any' type annotation can be improved
+// }
+
+interface SearchRepositories {
+  type: ActionType.SEARCH_REPOSITORIES;
 }
 
-const repositoriesReducer = (state: RepositoryState, action: Action): RepositoryState => {
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+enum ActionType {
+    SEARCH_REPOSITORIES='search_repositories',
+    SEARCH_REPOSITORIES_SUCCESS='search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR='search_repositories_error'
+}
+
+type Action =
+  | SearchRepositories
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
+const repositoriesReducer = (
+  state: RepositoryState,
+  action: Action
+): RepositoryState => {
+
+  /** Type Guard */
+  if (action.type === "search_repositories_success") {
+    console.log(action) // if you check the action's type it will show SearchRepositoriesSuccessAction
+  }
+  /************* */
   switch (action.type) {
-    case 'search_repositories':
-      return { loading: true, error: null, data: [] }
-    case 'search_repositories_success':
-      return { loading: false, error: null, data: action.payload }
-    case 'search_repositories_error':
-      return { loading: false, error: action.payload, data: [] }
+    case "search_repositories":
+      return { loading: true, error: null, data: [] };
+    case "search_repositories_success":
+      return { loading: false, error: null, data: action.payload };
+    case "search_repositories_error":
+      return { loading: false, error: action.payload, data: [] };
     default:
-      return state
+      return state;
   }
 };
 
