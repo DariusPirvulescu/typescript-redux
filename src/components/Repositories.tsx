6cea2df32f4fdd1c067d6846ca74788316be019a
Darius Pirvulescu <organicdarius@gmail.com>
2021-02-18 18:01:07 +0200

    Add useSelector to access state in Repositories

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux'; // same as mapStateToProps for class components
 
 import { useActions } from '../custom-hooks/useActions';
 
 const Repositories: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions()
+  const state = useSelector((state: any) => state.repositories)
+  // { loading, error, data }
+
+  console.log(state);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
